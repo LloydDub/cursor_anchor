6a989c7606a81fca410749eba5a9343006c237f7
Lloyd Worth <74573525+LloydDub@users.noreply.github.com>
2025-06-25 21:15:06 -0600

    re-setting up the project previous state

---
+++ cursor_anchor/cursor_anchor/cursor_anchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_anchorApp.swift
+//  cursor_anchor
+//
+//  Created by Lloyd Worth on 2025-06-25.
+//
+
+import SwiftUI
+
+@main
+struct cursor_anchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
